6f189aafd8a43b5a65bd275a84b021bc3f809a9e
고영준 <candybox@hivelab.co.kr>
2022-04-20 22:16:09 +0900

    feat($compile): make primitive wrapper class
    make primitive wrapper class
    
    - Distance
    - Move
    - Name
    
    prioritization

---
+++ src/main/java/racingcar/Distance.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Distance {
+    private final String distance;
+
+    public Distance( String distance ){
+        this.validateDistanceRule( distance );
+        this.distance = distance;
+    }
+
+    private void validateDistanceRule(String distance){
+
+    }
+}
